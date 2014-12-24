eb54e0972f96446273b6ce634e459942f513815a
Marc-André Brochu <marcandrebrochu@icloud.com>
2014-12-24 12:36:21 -0500

    Initial Commit

---
+++ turbobrain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  turbobrain
+//
+//  Created by Marc-André Brochu on 2014-12-24.
+//  Copyright (c) 2014 Marc-Andre Brochu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
